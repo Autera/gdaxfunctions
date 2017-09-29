4114f84ef8e73e74e3649992a7f3bb3e3093e166
Autera <j.ipp582@gmail.com>
2017-09-29 13:57:52 -0400

    .

---
+++ order/function.js
@@ -0,0 +1,33 @@
+{
+
+"disabled": false,
+
+"bindings": [
+
+{
+
+"authLevel": "function",
+
+"type": "httpTrigger",
+
+"direction": "in",
+
+"name": "req"
+
+},
+
+{
+
+"type": "http",
+
+"direction": "out",
+
+"name": "res"
+
+}
+
+]
+
+}
+
+
